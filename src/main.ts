e9bf585075d29deee04ab9c61f34eaff770bf038
Tobias Pickel <tobias.pickel@rsu-reifen.de>
2016-05-26 16:56:01 +0200

    imported map and http

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import { HTTP_PROVIDERS } from '@angular/http';
+import 'rxjs/add/operator/map'
 import { enableProdMode } from '@angular/core';
 import { NG2BlogAppComponent, environment } from './app/';
 
@@ -6,4 +8,4 @@ if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(NG2BlogAppComponent);
+bootstrap(NG2BlogAppComponent, [HTTP_PROVIDERS]);
