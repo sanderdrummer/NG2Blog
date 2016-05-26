83ca6bf0a579fe49b69fc13442e96ab6d11c44dc
Tobias Pickel <tobias.pickel@rsu-reifen.de>
2016-05-26 16:56:12 +0200

    added wp api function for get post and page

--- src/app/wp-api-service.service.ts
+++ src/app/wp-api-service.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Post } from './shared/post';
+
 
 @Injectable()
 export class WpApiServiceService {
 
-  constructor() {}
+  http:Http;
+  constructor(http:Http) {
+      this.http = http;
+  }
 
+  getPosts() {
+      return this.http
+          .get('http://localhost:3000/wordpress/wp-json/wp/v2/posts?category=all')
+          .map(res => res.json())
+          // .share();
+  }
+
+  getPage(id) {
+      return this.http
+          .get('http://localhost:3000/wordpress/wp-json/wp/v2/pages/' + id)
+          .map(res => new Post(res.json()));
+          // .share();
+  }
 }
