31ace3f9a17140083959c3e0589a8a894ee86c14
Tobias Pickel <tobias.pickel@rsu-reifen.de>
2016-05-26 16:56:38 +0200

    completed simple show page

--- src/app/+page/page.component.ts
+++ src/app/+page/page.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { WpApiServiceService } from '../wp-api-service.service';
+import { Post } from '../shared/post';
 
 @Component({
   moduleId: module.id,
   selector: 'app-page',
   templateUrl: 'page.component.html',
-  styleUrls: ['page.component.css']
+  styleUrls: ['page.component.css'],
+  providers: [WpApiServiceService],
 })
 export class PageComponent implements OnInit {
-
-  constructor() {}
-
-  ngOnInit() {
+  wpApiServiceService:WpApiServiceService;
+  page:Post;
+  constructor(wpApiServiceService:WpApiServiceService) {
+    this.wpApiServiceService = wpApiServiceService;
+      this.page = new Post({});
   }
 
+  ngOnInit() {
+      this.wpApiServiceService.getPage(2).subscribe((res) => {this.page = res; console.log(this.page );});
+
+  }
 }
