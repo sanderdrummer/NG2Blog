8e8697ba6e50b58783e36b2255753038f4a9aa48
sanderdrummer <funkpunkdrummer@gmail.com>
2016-07-23 14:27:08 +0200

    added child routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {Control} from '@angular/common';
 import {Observable} from 'rxjs/Observable';
 import {ROUTER_DIRECTIVES} from '@angular/router';
+import {SelectedPostService} from '../components/Post/postService';
 
 @Component({
     selector: 'app',
@@ -9,15 +10,16 @@ import {ROUTER_DIRECTIVES} from '@angular/router';
     <nav>
         <a href=""></a>
         <a [routerLink]="['/home']">Home</a>
-        <a [routerLink]="['/posts']">Posts</a>
-        <a [routerLink]="['/posts', 'Band']">Band</a>
-        <a [routerLink]="['/posts','Gigs']">Gigs</a>
+        <a [routerLink]="['/posts', 'blog']">Blog</a>
+        <a [routerLink]="['/posts', 'band']">Band</a>
+        <a [routerLink]="['/posts','gigs']">Gigs</a>
         <a [routerLink]="['/page/2']">Test</a>
         <a [routerLink]="['/media']">Medien</a>
 </nav>
     <router-outlet></router-outlet>
 </div>`,
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES],
+    providers: [SelectedPostService]
 })
 export class AppComponent {
 
